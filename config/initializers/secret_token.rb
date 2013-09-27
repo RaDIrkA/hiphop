1eaf2dce0e8781d52a9674f1a6f6e75c16704b26
Andraz Kuret <rex007@gmail.com>
2013-09-27 18:32:33 +0200

    project for viewing funny videos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiphop::Application.config.secret_key_base = 'e635574965d16d5021c5d941b3d1837c796d325998ed720752c86839fd97e07b11779605e6019f9e2d18bf1b574ff9b577d2fba5f1ff1f51c14aa536918aa448'
