318b2691f53bbdcda869b3c0a004fb02cb6c422d
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-09-30 17:36:10 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,7 @@ private
 		@current_user ||= FbUser.find(session[:user_id]) if session[:user_id]
 	end	
 
-	before_filter :any_user_in
 
-	def require_user
-  		redirect_to root_url unless any_user_in
-	end
 
 	helper_method :any_user_in
 
