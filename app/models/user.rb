9fee20172ad6bbdd8ffd8725919fceee4cc26310
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-09-30 15:11:16 +0200

    hiphop

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
  
 	def self.from_omniauth(auth)
 		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-			user.provider = auth.provider
+			user = auth.provider
 			user.uid = user.uid
 			user.name = auth.info.name
 			user.oauth_token = auth.credentials.token
