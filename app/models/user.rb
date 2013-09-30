f861283db53b9e1a26b5327d959f3d64e657367f
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-09-30 15:57:57 +0200

    eedit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	def self.from_omniauth(auth)
 		where(auth.slice(:provider, :uid)).first_or_initialize.tap do |users|
 			users = auth.provider
-			users.uid = users.uid
+			users.uid = user.uid
 			users.name = auth.info.name
 			users.oauth_token = auth.credentials.token
 			users.oauth_expires_at = Time.at(auth.credentials.expires_at)
