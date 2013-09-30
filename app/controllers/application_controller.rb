508d4fcada4c4795c1e2cb6841d0fb711ca755d5
Blaz Sajovic <blaz.sajovic@spletko.eu>
2013-09-30 17:40:02 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,12 @@ private
 	
 	def any_user_in
 		@any_user_in ||= User.find(session[:user_id]) if session[:user_id]
-		@current_user ||= FbUser.find(session[:user_id]) if session[:user_id]
+		
 	end	
 
-
+	def current_user
+		@current_user ||= FbUser.find(session[:user_id]) if session[:user_id]
+	end
 
 	helper_method :any_user_in
 
